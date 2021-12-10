5506852b535d698c627f2e21018e6047996b6349
APalazzetti <alexpalazzetti@gmail.com>
2021-12-10 15:45:22 +0100

    Lista film e listener

--- app/src/main/java/com/example/listastatieuropei5b/MainActivity.java
+++ app/src/main/java/com/example/listastatieuropei5b/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.listastatieuropei5b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listaf;
+    Button btnEsci;
+    String[] Film = {"Spiderman","Top gun","Megamind","Frankenstein"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaf=(ListView)findViewById(R.id.listaFilm);
+        btnEsci =(Button)findViewById(R.id.btnEsci);
+        ArrayAdapter<String> aaFilm = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Film);
+        listaf.setAdapter(aaFilm);
+
+        btnEsci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Stai per uscire",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
